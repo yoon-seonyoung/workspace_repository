c7040cffb3836695a845f943fc1837ed2b7194c4
윤선영 <cherhys12@gmail.com>
2026-01-13 00:42:01 +0900

    fix: (closed #5) v-for를 사용해서 피드리스트를 보여주는 메인페이지 구현

---
+++ src/components/FeedList.vue
@@ -0,0 +1,68 @@
+<template>
+    <div class="feed-list-container">
+        <div class="feed-container" v-for="feed in feeds" :key="feed.id">
+            <div class="feed-header">
+                <div class="feed-content">{{ feed.content }}</div>
+                <button class="feed-delete-button">X</button>
+            </div>
+            <div class="feed-name">{{ feed.user }}</div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "FeedList",
+    props: {
+        feeds: {
+            type: Array,
+            required: true,
+        }
+    }
+}
+</script>
+
+<style>
+.feed-list-container {
+    height: 60vh;
+    overflow-y: auto;
+}
+
+.feed-container {
+    height: 80px;
+    background-color: white;
+    margin: 10px 0px;
+    color: black;
+    padding: 3%;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    border-radius: 10px;
+
+}
+
+.feed-header {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+}
+
+.feed-content {
+    padding: 1%;
+
+}
+
+.feed-delete-button {
+    background: none;
+    border: none;
+    cursor: pointer;
+}
+
+.feed-name {
+    text-align: right;
+    font-size: 12px;
+}
+</style>
+
+//issue
